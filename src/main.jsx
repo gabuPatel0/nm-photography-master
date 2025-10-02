b89bc20a4652bd50001a5943a94f1dc98f3cf490
gabuPatel0 <gabupatel0@gmail.com>
2025-10-02 10:51:47 +0530

    hosfnus

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/nm-photography-master">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
