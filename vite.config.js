7915e11db1e9602b0c53466d1d07586ce5a84aa5
gabuPatel0 <gabupatel0@gmail.com>
2025-10-02 10:41:46 +0530

    jkbsnajksl

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/nm-photography-master/',
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
